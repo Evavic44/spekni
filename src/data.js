ba3cb56b6ca003d498ec140747481a6b5d0669be
Victor Eke <evavic44@gmail.com>
2022-07-06 23:25:58 +0100

    :package: Add dummy api data

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const cats = [
+  {
+    id: 1,
+    name: "John Boyega",
+    job: "Frontend Developer",
+    endorsements: 2300,
+    favoured: false,
+    image: {
+      url: "https://res.cloudinary.com/rocketmeme/image/upload/v1657144819/Spekni/user-1_kknjns.png",
+      alt: "John Boyega",
+    },
+    description:
+      "John Boyega is an excellent Frontend Developer and one of the core maintainers of the React team with a passion for creating quality software using languages like React, JavaScript, NodeJS, Express, Remix, Etc",
+    color: "orange",
+    gender: "male",
+  },
+  {
+    id: 2,
+    name: "Bisola Olu",
+    job: "Flutter Developer",
+    endorsements: 20,
+    favoured: false,
+    image: {
+      url: "https://res.cloudinary.com/rocketmeme/image/upload/v1657144819/Spekni/user-2_oied7n.png",
+      alt: "Bisola Olu",
+    },
+    description:
+      "Bisola Olu is a Flutter Developer and educator who loves creating quality software using languages like Flutter, DartJS, Kotlin, and React Native",
+    color: "purple",
+    gender: "female",
+  },
+  {
+    id: 3,
+    name: "Karim Benzema",
+    job: "Senior Software Developer",
+    endorsements: 400,
+    favoured: false,
+    image: {
+      url: "https://res.cloudinary.com/rocketmeme/image/upload/v1657144819/Spekni/user-3_sqsxou.png",
+      alt: "Karim Benzema",
+    },
+    description:
+      "Karim Benzema is an Senior Software Developer and educator who loves creating quality software using languages like C++, Python, Java, and ObjectC",
+    color: "cyan",
+    gender: "male",
+  },
+  {
+    id: 4,
+    name: "Rebeccah Liam",
+    job: "Shopify Developer",
+    endorsements: 208,
+    favoured: false,
+    image: {
+      url: "https://res.cloudinary.com/rocketmeme/image/upload/v1657144819/Spekni/user-4_t1x47w.png",
+      alt: "Rebeccah Liam",
+    },
+    description:
+      "Rebeccah Liam is an Shopify developer and educator who loves creating quality software using languages like Shopify and WordPress",
+    color: "pink",
+    gender: "female",
+  },
+  {
+    id: 5,
+    name: "Emeka Paul",
+    job: "Android Developer",
+    endorsements: 68,
+    favoured: false,
+    image: {
+      url: "https://res.cloudinary.com/rocketmeme/image/upload/v1657144819/Spekni/user-5_prpty1.png",
+      alt: "Emeka Paul",
+    },
+    description:
+      "Emeka Paul is an Android developer and educator who loves creating quality software using languages like Flutter, DartJS, Kotlin, Swift, etc.",
+    color: "blue",
+    gender: "male",
+  },
+  {
+    id: 6,
+    name: "Sophia Turner",
+    job: "Backend Developer",
+    endorsements: 100,
+    favoured: false,
+    image: {
+      url: "https://res.cloudinary.com/rocketmeme/image/upload/v1657144820/Spekni/user-6_wj51gs.png",
+      alt: "Sophia Turner",
+    },
+    description:
+      "Sophia Turner is a Fullstack Backend developer and educator who loves creating quality software using languages like Python, NodeJS, C++, etc.",
+    color: "yellow",
+    gender: "female",
+  },
+];
