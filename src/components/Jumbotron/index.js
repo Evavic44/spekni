5bef103b98edc2b5fd329f24cc9b51cca3a18eaf
Victor Eke <evavic44@gmail.com>
2022-07-09 21:53:03 +0100

    :paintbrush: Update placeholder image

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -9,11 +9,12 @@ export default function index() {
       <main className={styles.jumbotron}>
         <div>
           <LazyLoadImage
-            src="https://res.cloudinary.com/victoreke/image/upload/v1657396621/Spekni/group_srgx0s.png"
+            src="https://res.cloudinary.com/victoreke/image/upload/v1657396621/Spekni/group.png"
             width={320}
             height={80}
+            effect="blur"
             alt="Group Image"
-            placeholderSrc="https://res.cloudinary.com/victoreke/image/upload/v1657398809/Spekni/placeholder_tyux4q.png"
+            placeholderSrc="https://res.cloudinary.com/victoreke/image/upload/v1657399791/Spekni/placeholder.svg"
           />
           <h2>Find the right developers in a matter of seconds</h2>
           <span>The trusted recruiting platform you&apos;ll ever need</span>
