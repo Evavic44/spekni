00ccfd5dd23de179cd93ee15657f52770017bd24
Spiff Jekey-Green <spiffjekeygreen@gmail.com>
2022-07-10 05:44:18 +0100

    :lady_beetle: Accepted changes for merge pull

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -15,13 +15,5 @@ export default NextAuth({
   // }
   jwt: {
     signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
-    async encode(params) {
-      // return a custom encoded JWT string
-      return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    async decode(params) {
-      // return a `JWT` object, or `null` if decoding failed
-      return {}
-    },
   }
 });
