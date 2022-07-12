9c2286b7e4e32df19a4f51e9bc69c9a0467ee1dc
Victor Eke <evavic44@gmail.com>
2022-07-12 11:26:56 +0100

    :robot: Add meta information

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,67 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta
+            name="keywords"
+            content="Developer, Endorsements, Hiring, Job, Victor Eke, Planetscale, Hashnode, Hackathon"
+          />
+          {/* Primary met tags */}
+          <title>
+            Spekni - Hiring and Freelance organization Recruiters can Trust
+          </title>
+          <meta
+            name="title"
+            content="Spekni - Hiring and Freelance organization Recruiters can Trust"
+          />
+          <meta
+            name="description"
+            content="Spekni is a hiring platform built for recruiters to find the best developers based on reputable endorsements."
+          />
+          <meta
+            name="author"
+            content="EKe Victor, Spiff Jekey-Green, Nicholas Ovunda"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://spekni.vercel.app/" />
+          <meta
+            property="og:title"
+            content="Spekni - Hiring and Freelance organization Recruiters can Trust"
+          />
+          <meta
+            property="og:description"
+            content="Spekni is a hiring platform built for recruiters to find the best developers based on reputable endorsements."
+          />
+          <meta
+            property="og:image"
+            content="https://user-images.githubusercontent.com/62628408/177588390-29fc8f28-2088-4209-98b4-677b84c0e700.jpg"
+          />
+
+          <meta
+            property="twitter:card"
+            content="https://user-images.githubusercontent.com/62628408/177588390-29fc8f28-2088-4209-98b4-677b84c0e700.jpg"
+          />
+          <meta property="twitter:url" content="https://spekni.vercel.app/" />
+          <meta
+            property="twitter:title"
+            content="Spekni - Hiring and Freelance organization Recruiters can Trust"
+          />
+          <meta
+            property="twitter:description"
+            content="Spekni is a hiring platform built for recruiters to find the best developers based on reputable endorsements."
+          />
+          <meta
+            property="twitter:image"
+            content="https://user-images.githubusercontent.com/62628408/177588390-29fc8f28-2088-4209-98b4-677b84c0e700.jpg"
+          />
+          {/* Favicon */}
+          <link rel="icon" href="/favicon.svg" />
+
+          {/* Font */}
           <link
             href="https://fonts.googleapis.com/css?family=Inter:100,200,300,regular,500,600,700,800,900&display=swap"
             rel="stylesheet"
