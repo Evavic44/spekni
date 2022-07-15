981d248262e2b556f1120ec829e7e1ad721cc73c
Victor Eke <evavic44@gmail.com>
2022-07-15 21:54:14 +0100

    :zap: Add search bar

--- src/pages/explore/index.js
+++ src/pages/explore/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import styles from "../../styles/Explore.module.css";
 import Navbar from "../../components/Navbar";
 import Card from "../../components/Card";
 import userData from "../../data";
@@ -24,15 +25,35 @@ export default function Explore() {
       </Head>
       <Navbar />
 
-      {userData.map((data) => (
-        <Card
-          key={data.id}
-          name={data.name}
-          job={data.job}
-          image={data.image}
-          endorsements={data.endorsements}
-        />
-      ))}
+      <header className={styles.hero}>
+        <h1 className="font-extrabold max-w-2xl mx-auto text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center">
+          Explore over <span className="highlight">50,000+</span> Developers
+        </h1>
+
+        <div>
+          <input
+            className={styles.searchBar}
+            type="search"
+            id="Search"
+            placeholder="E.g: Victor Eke"
+          />
+          <button type="submit" aria-label="Search Button">
+            <i class="icon fas fa-search"></i>
+          </button>
+        </div>
+      </header>
+
+      <section className={styles.profileContainer}>
+        {userData.map((data) => (
+          <Card
+            key={data.id}
+            name={data.name}
+            job={data.job}
+            image={data.image}
+            endorsements={data.endorsements}
+          />
+        ))}
+      </section>
     </div>
   );
 }
