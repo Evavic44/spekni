4f1cf562fe6f09b6c1d3daf925f0bad30eb342c5
Evavic44 <evavic44@gmail.com>
2022-07-21 01:01:27 +0100

    :balance_scale: Add google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script"
 
 class MyDocument extends Document {
   render() {
@@ -68,6 +69,21 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+
+        <script async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.GA_TRACKING_ID}', {
+        page_path: window.location.pathname,
+      });
+    `,
+          }} />
       </Html>
     );
   }
