88732929a2997debe3ef6ef08b30092a3a6c82f3
Victor Eke <evavic44@gmail.com>
2022-07-25 00:58:52 +0100

    :zap: Style edit account page

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import styles from "../../styles/Account.module.css";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
@@ -14,48 +16,65 @@ export default function Account() {
 
       <Navbar />
       <main className={styles.container}>
-        <div className="mb-8">
-          <h1 className="font-extrabold max-w-4xl text-4xl sm:text-5xl lg:text-5xl tracking-tight mb-3">
-            Edit Account
-          </h1>
-          <p className="mt-1 text-sm text-gray-600">
-            This information will be displayed publicly so be careful what you
-            share.
-          </p>
+        <h1 className="font-extrabold max-w-4xl text-4xl text-center sm:text-5xl lg:text-5xl tracking-tight mb-12">
+          Edit Account
+        </h1>
+
+        <div className="flex justify-center">
+          <LazyLoadImage
+            className="rounded-full"
+            src="https://res.cloudinary.com/victoreke/image/upload/v1657144819/Spekni/user-1_kknjns.png"
+            alt="John Boyega"
+            width={120}
+            height={120}
+            placeholderSrc="https://res.cloudinary.com/victoreke/image/upload/v1657357322/Spekni/placeholder_piuucr.svg"
+          />
         </div>
 
         <div>
-          <div>
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Personal Information
-            </h3>
-            <p className="mt-1 text-sm text-gray-600">
-              Fill in the boxes with your personal details
-            </p>
-          </div>
+          <h3 className="text-lg font-bold leading-6 mt-6">
+            Personal Information
+          </h3>
 
-          <div className="md:grid md:grid-cols-3 md:gap-6">
+          <div className="md:grid md:grid-cols-2 md:gap-6">
             <div className="mt-5 md:mt-0 md:col-span-2">
               <form action="#" method="POST">
-                <div className="py-5 bg-white space-y-6">
-                  <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
+                <div className="py-5 space-y-6">
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className="">
                       <label
                         htmlFor="edit account"
-                        className="block text-sm font-medium text-gray-700"
+                        className="block text-sm font-medium"
                       >
-                        Website
+                        Full Name
                       </label>
                       <div className="mt-1 flex rounded-md shadow-sm">
-                        <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                          http://
-                        </span>
                         <input
                           type="text"
                           name="edit account"
                           id="edit account"
-                          className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
-                          placeholder="www.example.com"
+                          className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md text-sm"
+                          value="John Boyega"
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <div className="">
+                      <label
+                        htmlFor="edit account"
+                        className="block text-sm font-medium"
+                      >
+                        Job Title
+                      </label>
+                      <div className="mt-1 flex rounded-md shadow-sm">
+                        <input
+                          type="text"
+                          name="edit account"
+                          id="edit account"
+                          className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md text-sm"
+                          placeholder="Senior Software Engineer"
+                          required
                         />
                       </div>
                     </div>
@@ -64,188 +83,111 @@ export default function Account() {
                   <div>
                     <label
                       htmlFor="about"
-                      className="block text-sm font-medium text-gray-700"
+                      className="block text-sm font-medium"
                     >
-                      About
+                      Bio
                     </label>
                     <div className="mt-1">
                       <textarea
                         id="about"
                         name="about"
                         rows={3}
-                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                        placeholder="you@example.com"
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full text-sm rounded-md p-5"
                         defaultValue={""}
+                        required
                       />
                     </div>
-                    <p className="mt-2 text-sm text-gray-500">
+                    <p className="mt-4 text-sm">
                       Tell us a little about yourself in 160 words or less.
                     </p>
                   </div>
                 </div>
-              </form>
-            </div>
-          </div>
-        </div>
 
-        <div className="hidden sm:block" aria-hidden="true">
-          <div className="py-5">
-            <div className="border-t border-gray-200" />
-          </div>
-        </div>
-
-        <div className="mt-10 sm:mt-0">
-          <div>
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Social Account
-            </h3>
-            <p className="mt-1 text-sm text-gray-600">
-              Your presence on the interent
-            </p>
-          </div>
-
-          <div className="md:grid md:grid-cols-3 md:gap-6">
-            <div className="mt-5 md:mt-0 md:col-span-2">
-              <form action="#" method="POST">
-                <div className="px-4 py-5 bg-white sm:p-6">
-                  <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="first-name"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        First name
-                      </label>
-                      <input
-                        type="text"
-                        name="first-name"
-                        id="first-name"
-                        autoComplete="given-name"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="last-name"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Last name
-                      </label>
-                      <input
-                        type="text"
-                        name="last-name"
-                        id="last-name"
-                        autoComplete="family-name"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-4">
-                      <label
-                        htmlFor="email-address"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Email address
-                      </label>
-                      <input
-                        type="text"
-                        name="email-address"
-                        id="email-address"
-                        autoComplete="email"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="country"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Country
-                      </label>
-                      <select
-                        id="country"
-                        name="country"
-                        autoComplete="country-name"
-                        className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                      >
-                        <option>United States</option>
-                        <option>Canada</option>
-                        <option>Mexico</option>
-                      </select>
-                    </div>
-
-                    <div className="col-span-6">
-                      <label
-                        htmlFor="street-address"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Street address
-                      </label>
-                      <input
-                        type="text"
-                        name="street-address"
-                        id="street-address"
-                        autoComplete="street-address"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                      <label
-                        htmlFor="city"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        City
-                      </label>
-                      <input
-                        type="text"
-                        name="city"
-                        id="city"
-                        autoComplete="address-level2"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                      <label
-                        htmlFor="region"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        State / Province
-                      </label>
-                      <input
-                        type="text"
-                        name="region"
-                        id="region"
-                        autoComplete="address-level1"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                      <label
-                        htmlFor="postal-code"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        ZIP / Postal code
-                      </label>
-                      <input
-                        type="text"
-                        name="postal-code"
-                        id="postal-code"
-                        autoComplete="postal-code"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
+                <div className="hidden sm:block max-w-3xl" aria-hidden="true">
+                  <div className="py-5">
+                    <div className="borderLight" />
                   </div>
                 </div>
-                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                  <button type="submit" className="btn btn-primary">
-                    Save
-                  </button>
+
+                <div className="mt-10 sm:mt-0">
+                  <h3 className="text-lg font-bold leading-6 my-6">
+                    Social Account
+                  </h3>
                 </div>
+
+                <div className="grid grid-cols-6 gap-6">
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium"
+                    >
+                      Portfolio URL
+                    </label>
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm text-sm rounded-md"
+                      placeholder="johnboyega.com"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="last-name"
+                      className="block text-sm font-medium"
+                    >
+                      LinkedIn URL
+                    </label>
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm text-sm rounded-md"
+                      placeholder="johnboyega"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium"
+                    >
+                      GitHub URL
+                    </label>
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm text-sm rounded-md"
+                      placeholder="johnboyega"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="last-name"
+                      className="block text-sm font-medium"
+                    >
+                      LinkedIn URL
+                    </label>
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm text-sm rounded-md"
+                      placeholder="johnboyega"
+                    />
+                  </div>
+                </div>
+
+                <button type="submit" className="mt-10 btn btn-dark w-full">
+                  Update Profile
+                </button>
               </form>
             </div>
           </div>
