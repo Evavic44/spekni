c49a591140c3918d6e3b8759f8667f53be185433
Victor Eke <evavic44@gmail.com>
2022-08-02 10:16:40 +0100

    :rocket: add more testimonials

--- src/feedback.js
+++ src/feedback.js
@@ -6,7 +6,7 @@ export default [
     },
     review:
       "Spekni is an amazing service for getting developers noticed for their contributions and assistance in the developer world.",
-    src: "https://twitter.com/spekni",
+    src: "https://twitter.com/evavic44/status/1554140452777086977",
   },
   {
     name: "Spiff Jekey Green",
@@ -15,7 +15,7 @@ export default [
     },
     review:
       "Practically a well thought off and excelent project. It's a big suprise nobody has thought about building something like this before.",
-    src: "https://twitter.com/spekni",
+    src: "https://twitter.com/evavic44/status/1554140452777086977",
   },
   {
     name: "Nicholas Ovunda",
@@ -24,7 +24,25 @@ export default [
     },
     review:
       "Quite amazed by the creativity the team's creativity. It's a big suprise nobody has thought about building something like this before.",
-    src: "https://twitter.com/spekni",
+    src: "https://twitter.com/evavic44/status/1554140452777086977",
+  },
+  {
+    name: "Franklin (🧱, 🚀)",
+    image: {
+      url: "https://pbs.twimg.com/profile_images/1382648697104781313/N_FJmalR_400x400.jpg",
+    },
+    review:
+      "I would recommend Spekni for every creative especially newbies as this will help boost their discovery amongst other talents and strengthen their PROOF OF SKILL.",
+    src: "https://twitter.com/frankiefab100/status/1554141474417254401?s=20&t=n1a8yboKMkbDqhKVpVo4Mg",
+  },
+  {
+    name: "Boluwatife✨- The Vue Knight 💚🤺",
+    image: {
+      url: "https://pbs.twimg.com/profile_images/1530865676562333697/ovgpxKYd_400x400.jpg",
+    },
+    review:
+      "This is definitely a very well thought out project and the user experience is top top notch. User interface? Really really sleek and smooth 🥺...",
+    src: "https://twitter.com/developerBolu/status/1554163671798091777?s=20&t=qscL1UwsY-GMQ1P0XU5tGg",
   },
   {
     name: "John Boyega",
@@ -33,7 +51,48 @@ export default [
     },
     review:
       "I usually don't leave reviews for services or products, but Spekni is a great idea and I love the UI. Kudos to the team.",
-    src: "https://twitter.com/spekni",
+    src: "https://twitter.com/evavic44/status/1554140452777086977",
+  },
+  {
+    name: "Rakesh Potnuru",
+    image: {
+      url: "https://eke.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1642690499101%2F2oBwGQrjl.png%3Fw%3D160%26h%3D160%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=256&q=75",
+    },
+    review: "Great project 💯",
+    src: "https://eke.hashnode.dev/spekni-a-recognition-platform-built-for-developer-endorsements",
+  },
+  {
+    name: "Mitolu",
+    image: {
+      url: "https://pbs.twimg.com/profile_images/1364930269447417865/OiwFYYZB_400x400.jpg",
+    },
+    review:
+      "Another fantastic project from @evavic44🌟 Do well to check it out",
+    src: "https://twitter.com/israelmitolu/status/1554316556074717184?s=20&t=qscL1UwsY-GMQ1P0XU5tGg",
+  },
+  {
+    name: "eedris #FreePalestine 🇵🇸",
+    image: {
+      url: "https://pbs.twimg.com/profile_images/1495844616981164033/79hl6gfG_400x400.jpg",
+    },
+    review: "Marvelous job, Eke! 👏",
+    src: "https://twitter.com/eedrxs/status/1554158995535020035?s=20&t=qscL1UwsY-GMQ1P0XU5tGg",
+  },
+  {
+    name: "STICKERS NG",
+    image: {
+      url: "https://pbs.twimg.com/profile_images/751441699436716034/-umdgqTs_400x400.jpg",
+    },
+    review: "Dope stuff as always",
+    src: "https://twitter.com/_akandeseun/status/1554159843673530368?s=20&t=qscL1UwsY-GMQ1P0XU5tGg",
+  },
+  {
+    name: "therevealer",
+    image: {
+      url: "https://pbs.twimg.com/profile_images/1549903347007848450/apxBMA5j_400x400.jpg",
+    },
+    review: "This is awesome 👌",
+    src: "https://twitter.com/therevealerDev/status/1554183758617747456?s=20&t=n1a8yboKMkbDqhKVpVo4Mg",
   },
   {
     name: "Bisola Tolu",
@@ -41,14 +100,22 @@ export default [
       url: "https://res.cloudinary.com/victoreke/image/upload/v1657144819/Spekni/user-2_oied7n.png",
     },
     review: "Great product! Kudos to the Spekni team.",
-    src: "https://twitter.com/spekni",
+    src: "https://twitter.com/evavic44/status/1554140452777086977",
+  },
+  {
+    name: "Ahmad Adejumo",
+    image: {
+      url: "https://eke.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1656686101437%2FNYT2xc5t_A.jpeg%3Fw%3D160%26h%3D160%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=256&q=75",
+    },
+    review: "This is an awesome project 👏",
+    src: "https://eke.hashnode.dev/spekni-a-recognition-platform-built-for-developer-endorsements",
   },
   {
     name: "Your Name",
     image: {
-      url: "https://avatars.dicebear.com/api/identicon/.svg",
+      url: "https://res.cloudinary.com/victoreke/image/upload/v1658398513/Spekni/badge.svg",
     },
     review: "Add your reviews here",
-    src: "https://forms.gle/MisTGbxSyw3Ji8hf7",
+    src: "https://twitter.com/evavic44/status/1554140452777086977",
   },
 ];
